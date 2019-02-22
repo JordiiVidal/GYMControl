cdc5e2c814acca0cdb548942db445d1800f6a0ca
DAM <dam@A22O05.local>
2019-02-22 10:57:47 +0100

    ui exercice

--- lib/main.dart
+++ lib/main.dart
@@ -25,58 +25,131 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-  VoidCallback _showPersBottomSheetCallBack;
 
-  Icon _icon = Icon(
-    Icons.play_arrow,
-    size: 45.0,
-  );
-  int _kilos = 0;
-  bool state = false;
+  List<Exercice> list = [
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0)),
+    Exercice(
+        name: 'Press de banca',
+        weight: 45,
+        start: false,
+        series: 4,
+        actualSeries: 0,
+        recoveryTime: false,
+        repetitions: 12,
+        icon: Icon(Icons.play_arrow, size: 40.0))
+  ];
 
-  void initState() {
-    super.initState();
-    _showPersBottomSheetCallBack = _showBottomSheet;
-  }
-
-  void _showBottomSheet() {
-    setState(() {
-      _showPersBottomSheetCallBack = null;
-    });
-
-    _scaffoldKey.currentState
-        .showBottomSheet((context) {
-          return Container(
-            height: 300.0,
-            color: Colors.red,
-            child: Center(child: Text('sasas')),
-          );
-        })
-        .closed
-        .whenComplete(() {
-          if (mounted) {
-            setState(() {
-              _showPersBottomSheetCallBack = _showBottomSheet;
-            });
-          }
-        });
-  }
-
-  void _showModalSheet() {
+  _showModalSheet(BuildContext context, int index) {
     showModalBottomSheet(
         context: context,
         builder: (builder) {
           return Container(
-            height: 300.0,
-            color: Colors.red,
-            child: Center(child: Text('sasas')),
+            height: 350.0,
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  'assets/gym_01.jpg',
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Center(
+                    child: Text(
+                      list[index].name,
+                      style: TextStyle(fontSize: 21.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         });
   }
 
-  Widget _containerStart() {
+  Widget _containerStart(int index) {
     return Container(
-      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
       color: Theme.of(context).accentColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -91,18 +164,120 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Container(
             child: Text(
-              '0 / 3',
-              style: TextStyle(color: Colors.white, fontSize: 17.0),
+              list[index].actualSeries.toString() +
+                  ' / ' +
+                  list[index].series.toString(),
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
             ),
           ),
           FloatingActionButton(
-            onPressed: () => {},
+            onPressed: () {
+              setState(() {
+                list[index].aumentarSeries();
+              });
+            },
             backgroundColor: Colors.grey[200],
             child: Text(
               '+1',
               style: TextStyle(color: Colors.black),
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _itemList(BuildContext context, int index) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border: BorderDirectional(
+            top: BorderSide(
+              color: Colors.grey[300],
+              width: 1.0,
+            ),
+            bottom: BorderSide(
+              color: Colors.grey[300],
+              width: 1.0,
+            ),
+          )),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+            child: ListTile(
+              onTap: () {
+                _showModalSheet(context, index);
+              },
+              leading: FlatButton(
+                child: list[index].icon,
+                onPressed: () {
+                  print('click');
+                  if (list[index].start) {
+                    setState(() {
+                      list[index].start = false;
+                      list[index].icon = Icon(
+                        Icons.play_arrow,
+                        size: 40.0,
+                      );
+                    });
+                  } else {
+                    setState(() {
+                      list[index].start = true;
+                      list[index].icon = Icon(
+                        Icons.stop,
+                        size: 40.0,
+                      );
+                    });
+                  }
+                },
+              ),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    list[index].repetitions.toString(),
+                    style: TextStyle(fontSize: 21.0),
+                  ),
+                  Text(
+                    'rep',
+                    style: TextStyle(fontSize: 10.0),
+                  )
+                ],
+              ),
+              title: Text(list[index].name),
+              trailing: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    list[index].weight = list[index].weight + 2;
+                  });
+                },
+                onLongPress: () {
+                  setState(() {
+                    list[index].weight = 0;
+                  });
+                },
+                child: CircleAvatar(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        list[index].weight.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        'kg',
+                        style: TextStyle(color: Colors.white, fontSize: 10.0),
+                      ),
+                    ],
+                  ),
+                  radius: 30.0,
+                ),
+              ),
+            ),
+          ),
+          list[index].start ? _containerStart(index) : Container()
         ],
       ),
     );
@@ -115,74 +290,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: BorderDirectional(
-                  top: BorderSide(
-                    color: Colors.grey[300],
-                    width: 1.0,
-                  ),
-                  bottom: BorderSide(
-                    color: Colors.grey[300],
-                    width: 1.0,
-                  ),
-                )),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-                  child: ListTile(
-                    onTap: _showModalSheet,
-                    leading: FlatButton(
-                      child: _icon,
-                      onPressed: () {
-                        if (state) {
-                          setState(() {
-                            state = false;
-                            _icon = Icon(
-                              Icons.play_arrow,
-                              size: 45.0,
-                            );
-                          });
-                        } else {
-                          setState(() {
-                            state = true;
-                            _icon = Icon(
-                              Icons.stop,
-                              size: 45.0,
-                            );
-                          });
-                        }
-                      },
-                    ),
-                    subtitle: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[Text('Repeticiones'), Text('12')],
-                    ),
-                    title: Text('Nombre'),
-                    trailing: GestureDetector(
-                      onHorizontalDragDown: (DragDownDetails detail) {
-                        print(detail);
-                      },
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        child: Text(
-                          '$_kilos kg',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        radius: 30.0,
-                      ),
-                    ),
-                  ),
-                ),
-                state ? _containerStart() : Container()
-              ],
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: _itemList,
+        scrollDirection: Axis.vertical,
+        itemCount: list.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
--- lib/models/exercice.dart
+++ lib/models/exercice.dart
@@ -4,11 +4,21 @@ import 'package:flutter/material.dart';
 class Exercice{
 
   final String name;
-  final int repetitions;
-  final int series;
-  final double time;
-  final double weight;
+  int repetitions;
+  int series;
+  int actualSeries;
+  int weight;
+  bool start;
+  bool recoveryTime;
+  Icon icon;
 
-  Exercice({ @required this.name, @required this.weight, @required this.repetitions, @required this.series, @required this.time});
+  Exercice({ @required this.name, @required this.weight, @required this.repetitions, @required this.series,@required this.actualSeries,@required this.start, @required this.recoveryTime, @required this.icon});
 
+  void aumentarSeries(){
+    if(this.actualSeries < this.series){
+      this.actualSeries++;
+    }else{
+      print('fin');
+    }
+  }
 }
